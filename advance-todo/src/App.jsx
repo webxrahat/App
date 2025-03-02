2c42320899631d82220be139ad4294cae3d4e0c3
Md Rahat Islam <islammdrahat308@gmail.com>
2025-03-02 06:23:14 +0600

    nav

--- advance-todo/src/App.jsx
+++ advance-todo/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router";
+import Home from "./components/Home";
 
 const App = () => {
- return <div className="bg-red-400">App</div>;
+ return (
+  <div>
+   <nav>
+    <ul>
+     <li>
+      <Link to="/">Home</Link>
+     </li>
+     <li>
+      <Link to="/useReducerA">UseReducer 1</Link>
+     </li>
+     <li>
+      <Link to="/useReducerB">UseReducer 2</Link>
+     </li>
+     <li>
+      <Link to="/useContext">UseContext</Link>
+     </li>
+    </ul>
+   </nav>
+
+   <Routes>
+    <Route path="/" element={<Home />} />
+   </Routes>
+  </div>
+ );
 };
 
 export default App;
