547b2bec03e7094f760732d94442e286ba26e0db
webxrahat <islammdrahat308@gmail.com>
2025-06-26 17:02:03 +0600

    context

--- mlbd/src/App.jsx
+++ mlbd/src/App.jsx
@@ -3,18 +3,19 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import MovieList from "./components/cine/MovieList";
 import Footer from "./components/Footer";
+import { MovieContext } from "./context";
 
 export default function App() {
- return (
-  <>
-   <Header />
-   <main>
-    <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem] mx-auto">
-     <Sidebar />
-     <MovieList />
-    </div>
-   </main>
-   <Footer />
-  </>
- );
+  return (
+    <MovieContext.Provider value={{}}>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem] mx-auto">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </MovieContext.Provider>
+  );
 }
