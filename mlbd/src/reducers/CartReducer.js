e6c99ab6cb9f706e34173e14c5133d02f3fdc773
Md Rahat Islam <islammdrahat308@gmail.com>
2025-06-27 20:58:59 +0600

    context

---
+++ mlbd/src/reducers/CartReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+ cartData: [],
+};
+
+const CartReducer = (state, action) => {
+ switch (action.type) {
+  case "ADD_TO_CART":
+   break;
+ }
+};
+
+export { initialState, CartReducer };
