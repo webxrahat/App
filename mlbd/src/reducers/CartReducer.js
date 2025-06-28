07f0aafd90c20b87969ee84768c0eec7e8163935
Md Rahat Islam <islammdrahat308@gmail.com>
2025-06-28 08:22:35 +0600

    reducer

--- mlbd/src/reducers/CartReducer.js
+++ mlbd/src/reducers/CartReducer.js
@@ -5,7 +5,18 @@ const initialState = {
 const CartReducer = (state, action) => {
  switch (action.type) {
   case "ADD_TO_CART":
+   return {
+    cartData: [...state.cartData, action.payload],
+   };
    break;
+  case "REMOVE_FROM_CART":
+   return {
+    ...state,
+    cartData: state.cartData.filter((item) => item.id !== action.payload.id),
+   };
+   break;
+  default:
+   return state;
  }
 };
 
