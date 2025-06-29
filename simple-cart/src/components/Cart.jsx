451741fa195f11097e264919aa3a067cc46c955e
Md Rahat Islam <islammdrahat308@gmail.com>
2025-06-29 22:54:39 +0600

    cart value

--- simple-cart/src/components/Cart.jsx
+++ simple-cart/src/components/Cart.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Checkout from "./Checkout";
 import CartItems from "./CartItems";
+import { ProductContext } from "../context";
 
 export default function Cart() {
-  return (
-    <div className="bg-white rounded-lg p-6 border border-gray-200">
-      <CartItems />
-      <Checkout />
-    </div>
-  );
+ const { cartData, setCartData } = useContext(ProductContext);
+ return (
+  <div className="bg-white rounded-lg p-6 border border-gray-200">
+   <CartItems cartData={cartData} setCartData={setCartData} />
+   <Checkout />
+  </div>
+ );
 }
