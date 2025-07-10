77f166eadee28e584059749e642b74f71c7965e8
Md Rahat Islam <islammdrahat308@gmail.com>
2025-07-10 21:21:03 +0600

    rt git commit -m rt git commit -m rt

--- reducer-recap/src/App.jsx
+++ reducer-recap/src/App.jsx
@@ -1,37 +1,38 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const inintialName = { taskName: "one task" };
+ const inintialName = [
+  { id: crypto.randomUUID(), taskName: "one task" },
+  { id: crypto.randomUUID(), taskName: "one task" },
+  { id: crypto.randomUUID(), taskName: "one task" },
+ ];
 
-  const [todos, setTodo] = useState([inintialName]);
-  const [input, setInput] = useState({});
+ const [todos, setTodo] = useState(inintialName);
+ const [inputValue, setInputValue] = useState("");
+ const [edit, setEdit] = useState(false);
 
-  const handleChange = (e) => {
-    const name = e.target.value;
-    setInput({ taskName: name });
-  };
+ const handleChange = (e) => {
+  setInputValue(e.target.value);
+ };
 
-  const handleAddTask = () => {
-    setTodo([...todos, input]);
-  };
-  console.log("input", input);
-  console.log("todos", todos);
+ const handleAddTask = () => {
+  setTodo([...todos, { taskName: inputValue }]);
+  setInputValue(" ");
+ };
 
-  return (
-    <>
-      <div>
-        <input onChange={handleChange} type="text" />
-
-        <button onClick={handleAddTask}>Submit</button>
-      </div>
-      <ul>
-        {todos.map((task, idx) => (
-          <li key={idx}>
-            {task.taskName} <button>edit</button> <button>delete</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+ return (
+  <>
+   <div>
+    <input value={inputValue} onChange={handleChange} type="text" />
+    <button onClick={handleAddTask}>Submit</button>
+   </div>
+   <ul>
+    {todos.map((task, idx) => (
+     <li key={idx}>
+      {task.taskName} <button>edit</button> <button>delete</button>
+     </li>
+    ))}
+   </ul>
+  </>
+ );
 }
-// https://chatgpt.com/share/686fabdb-58fc-800b-8c73-8c48a9fa0084
