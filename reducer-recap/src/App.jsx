43b6f195a382ed825d61df8243fb90b1ef85b35c
Md Rahat Islam <islammdrahat308@gmail.com>
2025-07-11 06:24:10 +0600

    oder

--- reducer-recap/src/App.jsx
+++ reducer-recap/src/App.jsx
@@ -3,16 +3,24 @@ import React, { useState } from "react";
 export default function App() {
  const inintialName = [
   { id: crypto.randomUUID(), taskName: "one task" },
-  { id: crypto.randomUUID(), taskName: "one task" },
-  { id: crypto.randomUUID(), taskName: "one task" },
+  { id: crypto.randomUUID(), taskName: "two task" },
+  { id: crypto.randomUUID(), taskName: "Three task" },
  ];
 
  const [todos, setTodo] = useState(inintialName);
  const [inputValue, setInputValue] = useState("");
  const [edit, setEdit] = useState(false);
 
- const handleChange = (e) => {
-  setInputValue(e.target.value);
+ const handleChange = (task) => {
+  setTodo(
+   todos.map((t) => {
+    if (t.id === task.id) {
+     return task;
+    } else {
+     return t;
+    }
+   })
+  );
  };
 
  const handleAddTask = () => {
@@ -29,7 +37,23 @@ export default function App() {
    <ul>
     {todos.map((task, idx) => (
      <li key={idx}>
-      {task.taskName} <button>edit</button> <button>delete</button>
+      {edit ? (
+       <>
+        {" "}
+        <input
+         value={task.taskName}
+         onChange={(e) => handleChange({ ...task, textName: e.target.value })}
+         type="text"
+        />
+        <button onClick={() => setEdit(false)}>save</button>
+       </>
+      ) : (
+       <>
+        {" "}
+        {task.taskName} <button onClick={() => setEdit(true)}>edit</button>{" "}
+        <button>delete</button>
+       </>
+      )}{" "}
      </li>
     ))}
    </ul>
